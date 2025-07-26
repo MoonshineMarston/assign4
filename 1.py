7b4d40968129e5093b3d0a3ed10835de666954ff
MoonshineMarston <moonshinemarston@gmail.com>
2025-07-26 23:46:28 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+try:
+    
+    with open(r"C:\Users\jaswa\OneDrive\Documents\sample.txt","r") as f1:
+        r1=f1.readline()
+        r2=f1.readline()
+        print(f"Line 1: {r1}\nLine 2: {r2}")
+except FileNotFoundError:
+    print(f"Error: The file 'sample.txt' was not found")
